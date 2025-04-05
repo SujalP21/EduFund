e1399844452d7fcb6ef48a75eb8acb293c787b4b
mohitupd20 <srm27mohit@gmail.com>
2025-04-05 23:40:56 +0530

    error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './utils/AuthContext';
-import './index.css';
+
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+// import React, { StrictMode } from "react";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+
+import React, {StrictMode} from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter,  } from "react-router-dom";
+import { AuthProvider } from "./utils/AuthContext";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    
+  </StrictMode>
 );
